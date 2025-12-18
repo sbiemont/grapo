8141f9d5cc5c6f9b56f64448a14c7fc6efc077e9
sbiemont <samuel.biemont@gmail.com>
2025-12-18 20:54:40 +0100

    fix: improve code

--- astar/priority_queue.go
+++ astar/priority_queue.go
@@ -1,32 +1,32 @@
 package astar
 
 // priorityQueue implements a priority queue for A* algorithm
-type priorityQueue []*node
+type priorityQueue[T comparable] []*node[T]
 
 // Len returns the length of the priority queue
-func (q priorityQueue) Len() int { return len(q) }
+func (q priorityQueue[T]) Len() int { return len(q) }
 
 // Less compares two items in the queue
-func (q priorityQueue) Less(i, j int) bool {
+func (q priorityQueue[T]) Less(i, j int) bool {
 	return q[i].f < q[j].f
 }
 
 // Swap swaps two items in the priority queue
-func (q priorityQueue) Swap(i, j int) {
+func (q priorityQueue[T]) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i
 	q[j].index = j
 }
 
 // Push adds an item to the priority queue
-func (q *priorityQueue) Push(x any) {
-	item := x.(*node)
+func (q *priorityQueue[T]) Push(x any) {
+	item := x.(*node[T])
 	item.index = len(*q)
 	*q = append(*q, item)
 }
 
 // Pop removes and returns the item with the highest priority
-func (q *priorityQueue) Pop() any {
+func (q *priorityQueue[T]) Pop() any {
 	old := *q
 	n := len(old)
 	item := old[n-1]
